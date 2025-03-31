a2dc43610d7dc748cf58e9cf53185db8310712ee
himanshu7437 <himanshujangra368@gmail.com>
2025-03-31 23:59:26 +0530

    conf updated

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,10 +4,13 @@ const conf = {
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteProfileCollectionId: (import.meta.env.VITE_APPWRITE_PROFILE_COLLECTION_ID),
+    appwriteLikesCollectionId: (import.meta.env.VITE_APPWRITE_LIKES_COLLECTION_ID),
+    appwriteCommentsCollectionId: (import.meta.env.VITE_APPWRITE_COMMENTS_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
     appwriteBucket2Id: String(import.meta.env.VITE_APPWRITE_AVATARS_BUCKET_ID),
     appwriteTinyMce: String(import.meta.env.VITE_TINYMCE),
-    defaultAvatarId: String(import.meta.env.VITE_APPWRITE_DEFAULT_AVATAR)
+    defaultAvatarId: String(import.meta.env.VITE_APPWRITE_DEFAULT_AVATAR),
+    appwriteFrontendUrl: String(import.meta.env.VITE_APPWRITE_FRONTEND_URL)    
 }
 
 export default conf
